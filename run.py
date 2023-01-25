302ecbd348af0891f8ef1944b9324a2fbb3eedb3
CalumRiot <calumgriffin@yahoo.com>
2023-01-25 13:41:00 +0000

    add print function for game board giving top row an A-H Value set

--- run.py
+++ run.py
@@ -10,3 +10,15 @@ player_guess_grid = [[" "] * 8 for i in range(8)]
 computer_guess_grid = [[" "] * 8 for i in range(8)]
 grid_values = {'A':0, 'B':1, 'C':2, 'D':3, 'E':4, 'F':5, 'G':6, 'H':7}
 
+# Create Game board
+
+def print_board(board):
+    print("  A B C D E F G H")
+    print("  ---------------")
+    row_number = 1
+    for row in board:
+        print("%d|%s|" % (row_number, "|".join(row)))
+        row_number += 1
+
+
+
